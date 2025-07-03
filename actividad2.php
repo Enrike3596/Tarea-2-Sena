92298c2b53254638753bffac2879209bbd50ae29
Enrike3596 <luisenriquecuellarvelasquez@gmail.com>
2025-07-03 13:39:26 -0500

    Add files via upload

--- actividad2.php
+++ actividad2.php
@@ -27,6 +27,8 @@
     $nombres = array("Juan", "Pedro", "Luis", "Ana", "Maria");
     $apellidos = array("Perez", "Gonzalez", "Lopez", "Gomez", "Rodriguez");
     $edades = array(20, 25, 30, 35, 40);
+    $direcciones = array("Calle 65 # 48-50", "Calle 45 # 34-56", "Calle 23 # 12-34", "Calle 78 # 90-12", "Calle 34 # 56-78");
+    $telefonos = array("123456789", "987654321", "456789123", "789123456", "321654987");
     $fecha_cumpleaños = array("01/01/2005", "02/02/2000", "03/03/1995", "04/04/1990", "05/05/1985");
     $significado_color = array("Riqueza y Alegría.","Amor y Amistad.","Paz y Tranquilidad.","Creatividad y Optimismo.","Energía y Vitalidad.");
     $colores = array("Rojo", "Azul", "Verde", "Amarillo", "Naranja");
@@ -37,6 +39,8 @@
             <th>Nombre</th>
             <th>Apellido</th>
             <th>Edad</th>
+            <th>Dirección</th>
+            <th>Teléfono</th>
             <th>Fecha de Cumpleaños</th>
             <th>Color Favorito</th>
             <th>Significado del Color</th>
@@ -48,6 +52,8 @@
         echo "<td>$nombre</td>";
         echo "<td>{$apellidos[$i]}</td>";
         echo "<td>{$edades[$i]}</td>";
+        echo "<td>{$direcciones[$i]}</td>";
+        echo "<td>{$telefonos[$i]}</td>";
         echo "<td>{$fecha_cumpleaños[$i]}</td>";
         echo "<td>{$colores[$i]}</td>";
         echo "<td>{$significado_color[$i]}</td>";
